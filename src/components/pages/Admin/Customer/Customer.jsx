780d0b3cefc84e2f82304ecb57464906fb475629
Trong Khanh Mai <maitrongkhanh7@gmail.com>
2022-09-04 21:02:18 +0700

    feat: add customer page

---
+++ src/components/pages/Admin/Customer/Customer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.scss";
+import CustomerTable from "./CustomerTable/CustomerTable";
+
+export default function Customer() {
+  return <div className="customer">
+   <CustomerTable/>
+  </div>;
+}
