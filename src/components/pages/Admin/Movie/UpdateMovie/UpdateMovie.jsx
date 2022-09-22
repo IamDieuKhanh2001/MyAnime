3e5797e1be90d54110e9d09097622cb11940fe7e
Trong Khanh Mai <maitrongkhanh7@gmail.com>
2022-09-22 21:30:47 +0700

    fix: update

--- src/components/pages/Admin/Movie/UpdateMovie/UpdateMovie.jsx
+++ src/components/pages/Admin/Movie/UpdateMovie/UpdateMovie.jsx
@@ -5,35 +5,44 @@ import * as Yup from "yup";
 import SelectField from "./../CustomSelect/CustomSelect";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingAnimation from "../../../../global/LoadingAnimation/LoadingAnimation";
-import { APIAddMovie, APIGetMovieCategories, APIUpdateMovie } from "../../../../../api/axios/adminAPI";
+import {
+    APIAddMovie,
+    APIGetMovieCategories,
+    APIUpdateMovie,
+} from "../../../../../api/axios/adminAPI";
 import { toast } from "react-toastify";
 import { adminActions } from "../../../../../api/redux/slices/adminSlice";
-import _ from "lodash"
+import _ from "lodash";
 import { Dialog } from "@mui/material";
 
 export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
-    const [loadCategory, setLoadCategory] = useState()
+    const [loadCategory, setLoadCategory] = useState();
     const [formValues, setFormValues] = useState(null);
     const dispatch = useDispatch();
-    const categories = useSelector(state => state.admin.movieCategories)
-    const isShowModalUpdateMovie = useSelector(state => state.admin.isShowModalUpdateMovie)
-    let isInvalidUpdateMovie = useSelector(state=>state.admin.isInvalidUpdateMovie)
-    let movies = useSelector(state => state.admin.movies)
+    const categories = useSelector((state) => state.admin.movieCategories);
+    const isShowModalUpdateMovie = useSelector(
+        (state) => state.admin.isShowModalUpdateMovie
+    );
+    let isInvalidUpdateMovie = useSelector(
+        (state) => state.admin.isInvalidUpdateMovie
+    );
+    let movies = useSelector((state) => state.admin.movies);
     const initialValues = {
         name: movie?.title,
         studioName: movie?.studioName,
-        category: movie.categoryData.map(category=>{
-            return ({
+        category: movie?.categoryData?.map((category) => {
+            return {
                 value: category.id,
                 label: category.name,
-
-            }) 
+            };
         }),
     };
 
     const validationSchema = Yup.object().shape({
         name: Yup.string().max(50, "Up to 50 characters").required("Empty"),
-        studioName: Yup.string().max(50, "Up to 50 characters").required("Empty"),
+        studioName: Yup.string()
+            .max(50, "Up to 50 characters")
+            .required("Empty"),
         //category: Yup.array().max(3, "Max category is 3").required("Empty")
     });
 
@@ -42,42 +51,43 @@ export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
             const resUpdate = await APIUpdateMovie({
                 title: fields.name,
                 studioName: fields.studioName,
-                id: movie.id
-            })
+                id: movie.id,
+            });
             if (resUpdate?.status === 200) {
-                console.log(resUpdate)
-                const updatedMovie = resUpdate.data.data
-                let index = _.findIndex(movies, { id: movie.id })
+                console.log(resUpdate);
+                const updatedMovie = resUpdate.data.data;
+                let index = _.findIndex(movies, { id: movie.id });
                 let temp = [...movies];
                 temp[index] = updatedMovie;
-                const updateMoviesAction = adminActions.updateMovies(temp)
-                dispatch(updateMoviesAction)
-                toast.success(`Update movie success`)
-                hideDiaglogUpdate()
+                const updateMoviesAction = adminActions.updateMovies(temp);
+                dispatch(updateMoviesAction);
+                toast.success(`Update movie success`);
+                hideDiaglogUpdate();
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
     };
 
     const loadCategories = async () => {
-        setLoadCategory(true)
+        setLoadCategory(true);
         const resGetCategory = await APIGetMovieCategories();
         if (resGetCategory?.status === 200) {
-            const updateMovieCategoriesAction = adminActions.updateMovieCategories(resGetCategory.data)
+            const updateMovieCategoriesAction =
+                adminActions.updateMovieCategories(resGetCategory.data);
             dispatch(updateMovieCategoriesAction);
         }
-        setLoadCategory(false)
-    }
+        setLoadCategory(false);
+    };
     useEffect(() => {
-        loadCategories()
-        dispatch(adminActions.setIsUpdateMovie(true))
-        return ()=>{
-            dispatch(adminActions.setIsUpdateMovie(false))
-        }
-    }, [])
+        loadCategories();
+        dispatch(adminActions.setIsUpdateMovie(true));
+        return () => {
+            dispatch(adminActions.setIsUpdateMovie(false));
+        };
+    }, []);
     const handleClose = () => {
-        dispatch(adminActions.setShowModalUpdateMovie(false))
+        dispatch(adminActions.setShowModalUpdateMovie(false));
     };
     return (
         <>
@@ -95,7 +105,10 @@ export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
                                 <div className="card-body">
                                     <div className="addForm">
                                         <div className="mb-3">
-                                            <label className="small mb-1" htmlFor="inputName">
+                                            <label
+                                                className="small mb-1"
+                                                htmlFor="inputName"
+                                            >
                                                 Movie Name
                                             </label>
                                             <Field
@@ -106,9 +119,10 @@ export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
                                                 placeholder="Enter your movie name"
                                             />
                                             <span className="error">
-                                                {errors.name && touched.name && (
-                                                    <div>{errors.name}</div>
-                                                )}
+                                                {errors.name &&
+                                                    touched.name && (
+                                                        <div>{errors.name}</div>
+                                                    )}
                                             </span>
                                         </div>
                                         <div className="mb-3">
@@ -126,37 +140,57 @@ export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
                                                 placeholder="Enter your studio name"
                                             />
                                             <span className="error">
-                                                {errors.studioName && touched.studioName && (
-                                                    <div>{errors.studioName}</div>
-                                                )}
+                                                {errors.studioName &&
+                                                    touched.studioName && (
+                                                        <div>
+                                                            {errors.studioName}
+                                                        </div>
+                                                    )}
                                             </span>
                                         </div>
 
                                         <div className="mb-3">
-                                            <label className="small mb-1" htmlFor="inputCategory">
+                                            <label
+                                                className="small mb-1"
+                                                htmlFor="inputCategory"
+                                            >
                                                 Category
                                             </label>
-                                            {
-                                                loadCategory ? <LoadingAnimation /> : categories ? <Field
+                                            {loadCategory ? (
+                                                <LoadingAnimation />
+                                            ) : categories ? (
+                                                <Field
                                                     component={SelectField}
                                                     name="category"
-                                                    options={categories.map(category => {
-                                                        return ({
-                                                            value: category.id,
-                                                            label: category.name,
-
-                                                        })
-                                                    })}
-                                                /> : null
-                                            }
-                                            <span className="error" >
-                                                {errors.category && touched.category && (
-                                                    <div style={{ marginTop: 10 }}>{errors.category}</div>
-                                                )}
+                                                    options={categories.map(
+                                                        (category) => {
+                                                            return {
+                                                                value: category.id,
+                                                                label: category.name,
+                                                            };
+                                                        }
+                                                    )}
+                                                />
+                                            ) : null}
+                                            <span className="error">
+                                                {errors.category &&
+                                                    touched.category && (
+                                                        <div
+                                                            style={{
+                                                                marginTop: 10,
+                                                            }}
+                                                        >
+                                                            {errors.category}
+                                                        </div>
+                                                    )}
                                             </span>
                                         </div>
 
-                                        <button disabled={isInvalidUpdateMovie} className="btn btn-danger px-4" type="submit">
+                                        <button
+                                            disabled={isInvalidUpdateMovie}
+                                            className="btn btn-danger px-4"
+                                            type="submit"
+                                        >
                                             {isSubmitting && (
                                                 <span className="spinner-border spinner-border-sm mr-1"></span>
                                             )}
@@ -170,6 +204,5 @@ export default function UpdateMovie({ movie, hideDiaglogUpdate }) {
                 </Formik>
             </div>
         </>
-
     );
 }
