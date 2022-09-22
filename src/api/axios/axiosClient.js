eb4358b7014ff85a09cefd3c8236ffd36b48e955
Trong Khanh Mai <maitrongkhanh7@gmail.com>
2022-09-22 21:24:46 +0700

    fix: custom url

--- src/api/axios/axiosClient.js
+++ src/api/axios/axiosClient.js
@@ -2,22 +2,22 @@ import axios from "axios";
 import axiosRetry from "axios-retry";
 
 export const axiosClient = axios.create({
-  // baseURL: `https://myanime-heroku.herokuapp.com`,
-  baseURL: `http://localhost:8080`,
-  // headers: {
-  //   'content-type': 'application/json',
-  // },
+    baseURL: `https://myanime-heroku.herokuapp.com`,
+    //baseURL: `http://localhost:8080`,
+    // headers: {
+    //   'content-type': 'application/json',
+    // },
 });
 
 const retryDelay = (retryNumber = 0) => {
-  const seconds = Math.pow(2, retryNumber) * 1000;
-  const randomMs = 1000 * Math.random();
-  return seconds + randomMs;
+    const seconds = Math.pow(2, retryNumber) * 1000;
+    const randomMs = 1000 * Math.random();
+    return seconds + randomMs;
 };
 
 axiosRetry(axiosClient, {
-  retries: 2,
-  retryDelay,
-  // retry on Network Error & 5xx responses
-  retryCondition: axiosRetry.isRetryableError,
+    retries: 2,
+    retryDelay,
+    // retry on Network Error & 5xx responses
+    retryCondition: axiosRetry.isRetryableError,
 });
