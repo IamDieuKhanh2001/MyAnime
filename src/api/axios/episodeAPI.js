27e2887ed910d9223ef117b36c5940c764e23856
Trung <luahantinhthu9@gmail.com>
2022-11-05 15:55:12 +0700

    Request view ep with authorize

--- src/api/axios/episodeAPI.js
+++ src/api/axios/episodeAPI.js
@@ -2,8 +2,12 @@ import { axiosClient } from "./axiosClient";
 
 export const APIGetEpisodeBySeriesId = (seriesId) => {
   const url = `/episode/series/` + seriesId
+  const jwt = window.sessionStorage.getItem("jwt");
+  const headers = {
+      Authorization: `Bearer ${jwt}`,
+  };
   return axiosClient
-    .get(url)
+    .get(url, { headers: headers })
     .catch((err) => console.log("Can't call API after 2 retries", err));
 };
 
