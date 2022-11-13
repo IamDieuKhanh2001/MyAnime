2c56fbc45a0f07068980eb6a452681c8d64dc574
Trong Khanh Mai <maitrongkhanh7@gmail.com>
2022-11-13 23:28:02 +0700

    feat: User management

--- src/components/pages/Admin/Customer/CustomerTable/CustomerTable.jsx
+++ src/components/pages/Admin/Customer/CustomerTable/CustomerTable.jsx
@@ -1,108 +1,216 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+    APIBlockUser,
+    APIGetAllUser,
+    APIUnBlockUser,
+} from "../../../../../api/axios/adminAPI";
 import "./CustomerTable.scss";
+import ReactPaginate from "react-paginate";
+import LoadingAnimation from "../../../../global/LoadingAnimation/LoadingAnimation";
+import { toast } from "react-toastify";
+import { CircularProgress } from "@mui/material";
 
 export default function CustomerTable() {
-  const [show, setShow] = useState(false);
-  const [show1, setShow1] = useState(false);
-  const [show2, setShow2] = useState(false);
-  return (
-    <div className="customerTable">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="main-box no-header clearfix">
-              <div className="main-box-body clearfix">
-                <div className="table-responsive">
-                  <table className="table user-list">
-                    <thead>
-                      <tr>
-                        <th>
-                          <span>User</span>
-                        </th>
-                        <th>
-                          <span>Created</span>
-                        </th>
+    const [users, setUsers] = useState([]);
+    const [loadingAction, setLoadingAction] = useState({
+        status: false,
+        id: null,
+    });
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 5;
 
-                        <th>
-                          <span>Email</span>
-                        </th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img
-                            src="https://bootdey.com/img/Content/user_1.jpg"
-                            alt
-                          />
-                          <a href="" className="user-link">
-                            Mai Trọng Khánh
-                          </a>
-                        </td>
-                        <td className="dateCreated">2022/08/12</td>
-                        <td className="email">trongkhanh@gmail.com</td>
-                        <td style={{ width: "20%" }}>
-                        <button
-                        className={show?"buttonUnblock": "buttonBlock"}
-                        onClick={() => setShow(!show)}
-                    >
-                      {show ?"Unblock":"Block"}
-                    </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img
-                            src="https://bootdey.com/img/Content/user_3.jpg"
-                            alt
-                          />
-                          <a href="" className="user-link">
-                            Quách Diệu Khánh
-                          </a>
-                        </td>
-                        <td className="dateCreated">2021/08/02</td>
+    const loadAllUsers = async () => {
+        const res = await APIGetAllUser();
+        console.log(res.data);
+        setUsers(res.data);
+    };
+    useEffect(() => {
+        loadAllUsers();
+    }, []);
 
-                        <td className="email">trongkhanh@gmail.com</td>
-                        <td style={{ width: "20%" }}>
-                        <button
-                        className={show1?"buttonUnblock": "buttonBlock"}
-                        onClick={() => setShow1(!show1)}
-                    >
-                      {show1 ?"Unblock":"Block"}
-                    </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <img
-                            src="https://bootdey.com/img/Content/user_2.jpg"
-                            alt
-                          />
-                          <a href="" className="user-link">
-                            Nguyễn Thành Trung
-                          </a>
-                        </td>
-                        <td className="dateCreated">2021/10/30</td>
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = users.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(users.length / itemsPerPage);
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % users.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+    const blockUser = async (userId) => {
+        try {
+            setLoadingAction({
+                id: userId,
+                status: true,
+            });
+            const res = await APIBlockUser(userId);
+            await loadAllUsers();
+            console.log(res.data.message);
+            toast.success("Block user success");
+        } catch (e) {
+            toast.error("Block user fail");
+        } finally {
+            setLoadingAction({
+                id: null,
+                status: false,
+            });
+        }
+    };
+    const unBlockUser = async (userId) => {
+        try {
+            setLoadingAction({
+                id: userId,
+                status: true,
+            });
+            const res = await APIUnBlockUser(userId);
+            await loadAllUsers();
+            console.log(res.data.message);
+            toast.success("Unblock user success");
+        } catch (e) {
+            toast.error("Unblock user fail");
+        } finally {
+            setLoadingAction({
+                id: null,
+                status: false,
+            });
+        }
+    };
+    return (
+        <div>
+            <div className="customerTable">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="main-box no-header clearfix">
+                                <div className="main-box-body clearfix">
+                                    <div className="table-responsive">
+                                        {users.length > 0 ? (
+                                            <table className="table user-list">
+                                                <thead>
+                                                    <tr>
+                                                        <th>
+                                                            <span>User</span>
+                                                        </th>
+                                                        <th>
+                                                            <span>Created</span>
+                                                        </th>
 
-                        <td className="email">trongkhanh@gmail.com</td>
-                        <td style={{ width: "20%" }}>
-                        <button
-                        className={show2?"buttonUnblock": "buttonBlock"}
-                        onClick={() => setShow2(!show2)}
-                    >
-                      {show2 ?"Unblock":"Block"}
-                    </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                                                        <th>
+                                                            <span>Email</span>
+                                                        </th>
+                                                        <th>Actions</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {currentItems?.map(
+                                                        (user) => {
+                                                            return (
+                                                                <tr>
+                                                                    <td>
+                                                                        <img
+                                                                            src={
+                                                                                user.avatar
+                                                                                    ? user.avatar
+                                                                                    : "https://cdn.pixabay.com/photo/2017/01/25/17/35/picture-2008484_960_720.png"
+                                                                            }
+                                                                            alt
+                                                                        />
+                                                                        <a
+                                                                            href=""
+                                                                            className="user-link"
+                                                                        >
+                                                                            {
+                                                                                user.username
+                                                                            }
+                                                                        </a>
+                                                                    </td>
+                                                                    <td className="dateCreated">
+                                                                        {
+                                                                            user.createAt
+                                                                        }
+                                                                    </td>
+                                                                    <td className="email">
+                                                                        {
+                                                                            user.email
+                                                                        }
+                                                                    </td>
+
+                                                                    <td
+                                                                        style={{
+                                                                            width: "20%",
+                                                                        }}
+                                                                    >
+                                                                        {loadingAction.status &&
+                                                                        loadingAction.id ===
+                                                                            user.id ? (
+                                                                            <CircularProgress
+                                                                                key={
+                                                                                    user.id
+                                                                                }
+                                                                            />
+                                                                        ) : user.enable ? (
+                                                                            <button
+                                                                                className="buttonBlock"
+                                                                                onClick={() =>
+                                                                                    blockUser(
+                                                                                        user.id
+                                                                                    )
+                                                                                }
+                                                                            >
+                                                                                Block
+                                                                            </button>
+                                                                        ) : (
+                                                                            <button
+                                                                                className="buttonUnblock"
+                                                                                onClick={() =>
+                                                                                    unBlockUser(
+                                                                                        user.id
+                                                                                    )
+                                                                                }
+                                                                            >
+                                                                                Unblock
+                                                                            </button>
+                                                                        )}
+                                                                    </td>
+                                                                </tr>
+                                                            );
+                                                        }
+                                                    )}
+                                                </tbody>
+                                            </table>
+                                        ) : (
+                                            <LoadingAnimation />
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
+            <div className="customerPagniate">
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel=">>"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="<<"
+                    renderOnZeroPageCount={null}
+                    breakClassName={"page-item"}
+                    breakLinkClassName={"page-link"}
+                    containerClassName={"pagination"}
+                    pageClassName={"page-item"}
+                    pageLinkClassName={"page-link"}
+                    previousClassName={"page-item"}
+                    previousLinkClassName={"page-link"}
+                    nextClassName={"page-item"}
+                    nextLinkClassName={"page-link"}
+                    activeClassName={"active"}
+                />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
